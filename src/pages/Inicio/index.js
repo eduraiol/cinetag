8b96acd1ec4314444cafc1b80f978c328bdae3eb
Eduardo Raiol <marquesraiol@gmail.com>
2023-04-12 14:57:18 -0300

    Criando os componentes Card e Título

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,18 @@
 import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
+import Card from "../../components/Card";
 import Rodape from "../../components/Rodape";
+import Titulo from "../../components/Titulo";
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
             <Banner imagem="home"></Banner>
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
             <Rodape />
         </>
 
