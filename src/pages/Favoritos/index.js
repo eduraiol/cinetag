fb5f1cd7df6e5b2a1c691ca1fa2ea57dee2f26a1
Eduardo Raiol <marquesraiol@gmail.com>
2023-04-12 16:23:48 -0300

    componente Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,18 @@
+import Banner from 'components/Banner';
 import styles from './Favoritos.module.css';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
 
-function Favoritos(){
-    return(
+function Favoritos() {
+    return (
         <>
-            
+            <Banner imagem='favoritos'></Banner>
+            <Titulo>
+                <h1>Meus Favoritos!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id='1' titulo='Gatinhos' capa='https://thecatapi.com/api/images/get?format=src&type=png'/>
+            </section>
         </>
     )
 }
