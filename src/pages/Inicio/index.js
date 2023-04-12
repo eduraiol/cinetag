c2d5b5285b2b017e8178e1b7910fe38796630010
Eduardo Raiol <marquesraiol@gmail.com>
2023-04-12 14:09:47 -0300

    Primeiro commit da aplicação

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,16 @@
+import Banner from "../../components/Banner";
+import Cabecalho from "../../components/Cabecalho";
+import Rodape from "../../components/Rodape";
+
+function Inicio() {
+    return (
+        <>
+            <Cabecalho />
+            <Banner imagem="home"></Banner>
+            <Rodape />
+        </>
+
+    )
+}
+
+export default Inicio;
